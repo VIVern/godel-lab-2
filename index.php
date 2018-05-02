895bd804cc1830c51dd61539fb5565c8d4d5eca9
Denic Fedioukovitch <d.fedioukovitch@godeltech.com>
2018-05-02 18:10:48 +0300

    +genre view (now objects gets not only id but name)

--- index.php
+++ index.php
@@ -2,17 +2,21 @@
 
   require_once 'config/config.php';
 
-  $options = array(
-    'http'=>array(
+  $options = [
+    'http' => [
       'method' => "GET",
       'header' => 'Content-type: application/x-www-form-urlencoded'
-    )
-  );
+    ]
+  ];
 
   $context = stream_context_create($options);
 
   $query = json_decode(file_get_contents('https://api.themoviedb.org/3/movie/now_playing?api_key=e3c790bdb811cade513e875f4806841d&language=ru', false, $context), true);
   $result = $query['results'];
+
+  $query = json_decode(file_get_contents('https://api.themoviedb.org/3/genre/movie/list?api_key=e3c790bdb811cade513e875f4806841d&language=ru', false, $context), true);
+  $genre = $query['genres'];
+
 
   class Film
   {
@@ -21,8 +25,9 @@
     public $poster;
     public $overview;
     public $releaseDate;
-    //public $runtime;
+    //public $runtime; there is no such option;
     public $genresId;
+    public $genresList = [];
 
     public function __construct($param1, $param2, $param3, $param4, $param5, $param6)
     {
@@ -31,48 +36,46 @@
       $this->poster = $param3;
       $this->overview = $param4;
       $this->releaseDate = $param5;
-      $this->genersId = $param6;
+      $this->genresId = $param6;
     }
 
-    //protected $genresList = [];
-
-    // protected function getGeners($genersID)
-    // {
-    //   $options = array(
-    //     'http'=>array(
-    //       'method' => "GET",
-    //       'header' => 'Content-type: application/x-www-form-urlencoded'
-    //     )
-    //   );
-    //
-    //   $context = stream_context_create($options);
-    //
-    //   $query = json_decode(file_get_contents('https://api.themoviedb.org/3/genre/movie/list?api_key=e3c790bdb811cade513e875f4806841d&language=ru', false, $context), true);
-    //
-    //   for ($i = 0; $i < count($genersID); $i++) {
-    //     array_push($this->$genresList, $genersID[$i]);
-    //   }
-    // }
+    public function getGenres($genreArray)
+    {
+      echo "run method";
+      echo "<br>";
+      foreach ($this->genresId as $id) {
+        foreach ($genreArray as $val) {
+          if($val['id'] === $id) {
+            array_push($this->genresList, $val['name']);
+          }
+        }
+      }
+    }
   }
 
   $films = [];
 
   for ($i = 0; $i < count($result); $i++) {
     array_push($films, new Film($result[$i]['title'], $result[$i]['original_title'], $result[$i]['poster_path'], $result[$i]['overview'], $result[$i]['release_date'], $result[$i]['genre_ids']));
+    $films[$i]->getGenres($genre);
   }
+
+  print_r($films);
 
 
   // request to data base block;
-  print_r($films[0]->overview);
+  foreach ($films as $film) {
+    print_r($films[0]->overview);
 
-  $title = $films[0]->title;
-  $titleOriginal = $films[0]->titleOriginal;
-  $poster = $films[0]->poster;
-  $overview = $films[0]->overview;
-  $realeseDate = $films[0]->releaseDate;
-  $gener = $films[0]->genersId;
+    $title = $film->title;
+    $titleOriginal = $film->titleOriginal;
+    $poster = $film->poster;
+    $overview = $film->overview;
+    $realeseDate = $film->releaseDate;
+    $genre = $film->genresList;
 
 
-  $querry = "INSERT INTO films VALUES( NULL, '$title', '$titleOriginal' ,'$poster' , '$overview', '$realeseDate', '$gener')";
+    $querry = "INSERT INTO films VALUES( NULL, '$title', '$titleOriginal' ,'$poster' , '$overview', '$realeseDate', '$genre')";
 
-  $req=mysqli_query($db_con,$querry);
+    $req=mysqli_query($db_con,$querry);
+  }
