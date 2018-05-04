0b98ae30669ef000aa17fcec725114e0cd386c32
Denic Fedioukovitch <d.fedioukovitch@godeltech.com>
2018-05-04 14:16:04 +0300

    +logs +writer errors with database into log file

--- config/config.php
+++ config/config.php
@@ -1,4 +1,6 @@
 <?php
+  $log = fopen('./logs/log.txt', 'a');
+
   //подключение к базе
   $db_location = 'localhost';
   $db_user = 'root';
@@ -9,5 +11,10 @@
   mysqli_set_charset($db_con, 'utf8');
 
   if(!$db_con){
-    exit('error');
+    $text = date("Y-m-d H:m:s") . " fail to connect to database. check config.php\n";
+    fwrite($log, $text);
+    exit('Warning: check log file for more information');
+  } else {
+    $text = date("Y-m-d H:i:s") . " connected to database successfully\n";
+    fwrite($log, $text);
   }
--- deploy.php
+++ deploy.php
@@ -1,5 +1,6 @@
 <?php
-  require_once '../config/config.php';
+  require_once './config/config.php';
+  $log = fopen('logs/log.txt', 'a');
 
   $query = "CREATE TABLE films (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -11,4 +12,13 @@
     genres TINYTEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
   )";
 
-  mysqli_query($db_con, $query);
+  $response = mysqli_query($db_con, $query);
+
+  if($response === false) {
+    $text = date("Y-m-d H:m:s") . " failed to create table. Check your query and connection to database.\n";
+    fwrite($log, $text);
+    exit('Warning: check log file for more information');
+  } else {
+    $text = date("Y-m-d H:m:s") . " table was created successfully\n";
+    fwrite($log, $text);
+  }
