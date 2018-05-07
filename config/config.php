28ed2a904e6206b189387ff32a97f3a1330882a5
Denic Fedioukovitch <d.fedioukovitch@godeltech.com>
2018-05-07 12:55:05 +0300

    +clearing values from config file

--- config/config.php
+++ config/config.php
@@ -1,6 +1,6 @@
 <?php
-  $API_token = 'e3c790bdb811cade513e875f4806841d';
-  $db_location = 'localhost';
-  $db_user = 'root';
-  $db_pass = '123';
-  $db_name = 'lab2';
+  $API_token = '';
+  $db_location = '';
+  $db_user = '';
+  $db_pass = '';
+  $db_name = '';
