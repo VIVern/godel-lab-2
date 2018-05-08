9650a51f0c24ee14886b5f86c16450fcf42a1733
Denis Fedioukovitch <denis3901442@gmail.com>
2018-05-09 00:51:33 +0300

    +interface DataActions was removed +interface method was removed into abstract class Database

--- abstract_classes/Database.php
+++ abstract_classes/Database.php
@@ -2,4 +2,8 @@
   abstract class Database
   {
     abstract protected function connect();
+    abstract public function setData($data,$table);
+    abstract public function getData($table);
+    abstract public function removeData($table);
+    abstract public function updateData();
   }
--- interfaces/DataActions.php
+++
@@ -1,8 +0,0 @@
-<?php
-  interface DataActions
-  {
-    public function setData($data,$table);
-    public function getData($table);
-    public function removeData($table);
-    public function updateData();
-  }
